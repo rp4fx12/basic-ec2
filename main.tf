2c6609238ffbedb0eba8a6a920a3ced01802a67d
Rohan Prasad <roranj@gmail.com>
2017-07-04 13:44:33 -0400

    Clean code

--- main.tf
+++ main.tf
@@ -16,13 +16,8 @@ resource "aws_instance" "example" {
               chmod 755 /app
               cd /app
               git clone https://github.com/rp4fx12/hello-world.git
-              git status > test.txt
               curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
               sudo yum install nodejs --enablerepo=nodesource -y
-              #curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-              #echo . ~/.nvm/nvm.sh >> ~/.bashrc
-              #. ~/.bashrc
-              #nvm install 7.10.0
               node --version > nodeVersion.txt
               cd /app/hello-world
               npm install
